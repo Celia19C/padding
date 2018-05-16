3451f92797faa01f144fb239d0fd6e6b8b5437bf
Ele_hopper <elenacerezo83@gmail.com>
2018-05-16 11:48:35 +0200

    GREEN TEST 'should fill the word with characters before the word if the argument is bigger than the word length'

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ Si el tamaño final es menor o igual que la cadena inicial, se devuelve sin toca
 
 */
 
-function paddingLeft(word, number) {
+function paddingLeft(word, number, character) {
 	var wordSplitted = word.split();
 	var spaces = number - word.length;
 	if (spaces > 0) {
 		for (var i = 0; i < spaces; i++) {
-			wordSplitted.unshift(' ');
+			if (character) {
+				wordSplitted.unshift(character);
+			}
+			else {
+				wordSplitted.unshift(' ');
+
+
+			}
 		}
 		return wordSplitted.join('');
 	}
