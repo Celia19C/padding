792fef764781cd5f17aec0a0e34938901b70a62a
Alienah <aidalbla@gmail.com>
2018-05-16 11:34:49 +0200

    RED: should fill the space between the lenth of the word and the total number of characters with the specified in the third parameter

--- index.test.js
+++ index.test.js
@@ -10,4 +10,7 @@ describe('Padding-left App', () => {
     test('should fill the word with more spaces before the word if the argument is bigger than the word length', function () {
         expect(paddingLeft('hola', 8)).toBe('    hola');
     });
+    test('should fill the space between the lenth of the word and the total number of characters with the specified in the third parameter', function () {
+        expect(paddingLeft('hola', 6, '@')).toBe('@@hola');
+    });
 });
