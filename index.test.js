f1e32eb0125bd479b88a34f5eb01ba6f5928cfa3
Teresa <teresamarbancastro@gmail.com>
2018-05-16 09:34:44 +0200

    first expect returning a word

--- index.test.js
+++ index.test.js
@@ -1,4 +1,7 @@
 const paddingLeft = require('./index');
 
 describe ('Padding-left App', () => {
+    test('should return a word when given a word', function(){
+        expect(paddingLeft('hola')).toBe('hola');
+       });
 });
