22705665c7b4aabb0a93472d172d0cb497befbb3
Celia Conde <celia_conde@yahoo.es>
2018-05-16 11:40:53 +0200

    GREEN should fill the word with the third argument, that it is a character

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const paddingLeft = require('./index');
 
 describe('Padding-left App', () => {
-    test("should return the whole word when the numbers of characters are less", function () {
+    test("should return the whole word when the numbers of characters are less", function() {
         expect(paddingLeft("hola", 3)).toBe("hola");
     })
     test('should fill the word with spaces before the word if the argument is bigger than the word length', function () {
@@ -9,5 +9,8 @@ describe('Padding-left App', () => {
     });
     test('should fill the word with more spaces before the word if the argument is bigger than the word length', function () {
         expect(paddingLeft('hola', 8)).toBe('    hola');
+    });
+    test('should fill the word with the third argument, that it is a character', function() {
+        expect(paddingLeft('hola', 8, 'z')).toBe('zzzzhola');
     });
 });
