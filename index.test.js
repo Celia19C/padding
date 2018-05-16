957314f07f93e7300c1ff1909d19b13ffaeff6ad
Ele_hopper <elenacerezo83@gmail.com>
2018-05-16 10:10:30 +0200

    RED should fill the word with spaces before the word if the argument is bigger than the word length

--- index.test.js
+++ index.test.js
@@ -4,4 +4,7 @@ describe('Padding-left App', () => {
     test("should return the whole word when the numbers of characters are less", function () {
         expect(paddingLeft("hola", 3)).toBe("hola");
     })
+    test('should fill the word with spaces before the word if the argument is bigger than the word length', function () {
+        expect(paddingLeft('hola', 5)).toBe(' hola');
+    })
 });
